39f5d05051e0c6dfa1707a609eab3c9103b0a5ff
a23893 <a23893@alunos.ipca.pt>
2024-11-23 14:32:35 +0000

    paymentstatus created and working

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import paymentController from "../controllers/paymentController.js";
+import paymentStatusController from "../controllers/paymentStatusController.js";
 
 const router = Router();
 
 // PaymentStatus Routes
-
+router.get('/paymentstatus', paymentStatusController.getAllPaymentStatuses);
+router.get('/paymentstatus/:id', paymentStatusController.getPaymentStatusById);
+router.post('/paymentstatus', paymentStatusController.createPaymentStatus);
+router.put('/paymentstatus/:id', paymentStatusController.updatePaymentStatus);
+router.delete('/paymentstatus/:id', paymentStatusController.deletePaymentStatus);
 
 // Payment Routes
 router.get('/payments', paymentController.getAllPayments);
